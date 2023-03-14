e95dee331fdeba580c6e6694baceda676b11c1ad
Darki03 <jeanolivierbinon@yahoo.fr>
2023-03-14 23:02:45 +0100

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='enoceanjob',
-    version='0.60.1',
+    version='0.60.2',
     description='EnOcean serial protocol implementation',
     author='Kimmo Huoman',
     author_email='jo.binon@gmail.com',
